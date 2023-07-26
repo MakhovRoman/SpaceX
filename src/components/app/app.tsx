4a7f37524194952c510c0463b0ff0a77d3455803
Roman Makhov <nekromantos30888666@gmail.com>
2023-07-26 12:11:30 +0300

    first commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './app.module.scss';
+
+export const App:React.FC = () => {
+  return (
+    <main className={styles.main}>
+      HELLO WORLD!
+    </main>
+  )
+}
