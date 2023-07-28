f3aec29d1151d85a0e8be9e85c5f477abae1d026
Roman Makhov <nekromantos30888666@gmail.com>
2023-07-28 21:05:45 +0300

    fix: fix import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/main.scss';
-import { App } from './components/app/app';
+import { App } from './components/app/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
