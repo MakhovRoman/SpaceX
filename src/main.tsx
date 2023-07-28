1c2cfdf606a1a4285692e38ba681700571502d0a
Roman Makhov <nekromantos30888666@gmail.com>
2023-07-28 21:13:46 +0300

    fix: error import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './styles/main.scss';
 import { App } from './components/app/App';
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
